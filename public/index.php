fe364e5a0972b80f6e52ea106da5e38c0a729915
Rens Sikma <rens@atcomputing.nl>
2019-05-10 13:21:28 +0200

    commit 9

--- public/index.php
+++ public/index.php
@@ -78,7 +78,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   <!-- hier is alvast het eerste van totaal zeven wachtwoorden: Bootstrap_Rulez
   Dit wachtwoord was natuurlijk wel erg makkelijk te vinden. Dus, hup! Zoeken naar die andere zes....
-  versie 0.7
+  versie 0.9
   -->
   </body>
 </html>
